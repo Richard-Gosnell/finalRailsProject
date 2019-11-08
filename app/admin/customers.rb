8e0ec13f2b542be729392e1bfd17f7248d3838af
Richard-Gosnell <richardshanegosnell@gmail.com>
2019-11-08 11:20:31 -0600

    Resources built into active admin

---
+++ app/admin/customers.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Customer do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :customer_ID, :first_name, :last_name, :address, :order_ID
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:customer_ID, :first_name, :last_name, :address, :order_ID]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/orders.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Order do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :order_ID, :customer_ID, :taxes, :total_cost
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:order_ID, :customer_ID, :taxes, :total_cost]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
