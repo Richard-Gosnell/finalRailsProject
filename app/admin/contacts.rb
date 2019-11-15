3b7a7836e38afc3ae5e5e197ae2070b9e3ce9b29
Richard-Gosnell <richardshanegosnell@gmail.com>
2019-11-15 15:01:07 -0600

    Before new gem

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -1,18 +1,18 @@
-ActiveAdmin.register Contact do
+# frozen_string_literal: true
 
+ActiveAdmin.register Contact do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :phone, :email_address, :address, :city, :province, :country, :postal_code
+  permit_params :phone, :email_address, :address, :city, :province, :country, :postal_code
   #
   # or
   #
   # permit_params do
-  #   permitted = [:phone, :email_address, :address, :city, :province, :country, :postal_code]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #  permitted = %i[phone email_address address city province country postal_code]
+  #  permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
