d0db647401d63b91c2636ad7689866c0e7d414bd
Richard-Gosnell <richardshanegosnell@gmail.com>
2019-11-21 13:50:58 -0600

    About page up

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: true
 
 class AboutController < ApplicationController
-  resources :about
-
   def index
-    @abouts = About.all
-    # @abouts = About.find(params[:all])
-    # @abouts = About.find(params[:id])
-  end
-
-  def show
-    @abouts = About.find(params[:id])
+    @abouts = About.order('id')
   end
 end
--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class ContactController < ApplicationController
-  # @contacts = ContactController.find(params[:id])
-
-  # def _contact
-  #  @contacts = Contact.where('id = 1', params[:id])
-  # end
   def index
     @contacts = Contact.order('id')
   end
